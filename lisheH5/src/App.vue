42928487e95616bbec3320bc0cc8d8c0e94586c6
lbin233 <1872639868@qq.com>
2020-06-02 14:13:32 +0800

    h5端项目结构配置完成

--- lisheH5/src/App.vue
+++ lisheH5/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <tabbar></tabbar>
+    <div>
+      <keep-alive>
+        <router-view v-if="$route.meta.$keepAlive"></router-view>
+      </keep-alive>
+      <router-view v-if="!$route.meta.$keepAlive"></router-view>
     </div>
-    <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<script>
+import tabbar from './views/tabbar' 
+export default {
+  name:"app",
+  components:{
+    tabbar
+  }
+};
+</script>
+<style scope lang="less">
+// 基础样式
+@import url("./assets/css/base.less");
+// 字体样式
+@import url("./assets/font/iconfont.css");
 </style>
--- lisheH5/src/router/index.js
+++ lisheH5/src/router/index.js
@@ -1,22 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home/index.vue'
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:"",
+    redirect:"/home"
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:"/home",
+    component:Home
+  },
+  {
+    path:"/sort",
+    component:()=>import("../views/sort/index.vue")
+  },
+  {
+    path:"/cart",
+    component:()=>import("../views/cart/index.vue")
+  },
+  {
+    path:"/personal",
+    component:()=>import("../views/personal/index.vue")
   }
 ]
 
