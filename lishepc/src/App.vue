5b924817da7fb1fb3cb24f1eacc8bc5ae761cfe9
lbin233 <1872639868@qq.com>
2020-06-03 13:41:25 +0800

    上传了头部导航

--- lishepc/src/App.vue
+++ lishepc/src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
-    
+    <head-nav></head-nav>
   </div>
 </template>
-
-<style>
-
+<script>
+  import headNav from '_c/headNav.vue'
+  export default {
+    name:"app",
+    components:{
+      headNav
+    }
+  }
+</script>
+<style lang="less">
+@import url("./assets/css/base.less");
 </style>
