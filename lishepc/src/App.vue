7d05d6e3ac35b89784986b8a12acce1da7a8606c
cq741798819 <741798819@qq.com>
2020-06-03 14:23:34 +0800

    提交头部组件

--- lishepc/src/App.vue
+++ lishepc/src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div id="app">
-    
+    <top-Head></top-Head>
+    <footer></footer>
   </div>
 </template>
-
+<script>
+import topHead from './components/topHead'
+import footer from './components/footer'
+export default {
+  nam:'app',
+  components:{
+    topHead,
+    footer
+  }
+}
+</script>
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+}
 </style>
---
+++ lishepc/src/components/footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="footer">
+    <div class="footer_2_pr">
+      <div class="footer_2_tips_quan">
+        <p>全</p>
+        <span>商品齐全 随心挑选</span>
+      </div>
+      <div class="footer_2_tips_quan">
+        <p>全</p>
+        <span>全球优品 万里挑一</span>
+      </div>
+      <div class="footer_2_tips_quan">
+        <p>全</p>
+        <span>灵活兑换 购物无忧</span>
+      </div>
+      <div class="footer_2_tips_quan">
+        <p>全</p>
+        <span>专属内购 专享折扣</span>
+      </div>
+      <div class="footer_2_tips_quan">
+        <p>全</p>
+        <span>多仓直发 全国配送</span>
+      </div>
+      <div class="footer_2_tips_quan">
+        <p>全</p>
+        <span>极速响应 品质服务</span>
+      </div>
+    </div>
+    <div class="footer_2_bottom">
+      <div class="footer_2_bottom_a">
+        <a href="#">关于我们</a>
+        <a href="#">礼舍指南</a>
+        <a href="#">积分兑换</a>
+        <a href="#">关于我们</a>
+        <a href="#">关于我们</a>
+        <a href="#">关于我们</a>
+        <a href="#">关于我们</a>
+      </div>
+    </div>
+     <div class="footer_2_bottom_text">
+       <span>食品经营许可证：JY14403050238924</span>
+       <span>营业执照  礼舍科技版权所有  统一社会信用代码：91440300071136394Y</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "footer"
+};
+</script>
+
+<style lang="less" scope>
+.footer{
+    background: rgb(65,65,65);
+}
+</style>
