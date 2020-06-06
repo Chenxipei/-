8af5ed9c7818a322116406266d09dbfa1137d1f9
Q1075068834 <1075068834@qq.com>
2020-06-06 09:46:42 +0800

    周泽巨添加了版权底部组件

--- lishepc/src/views/login/register.vue
+++ lishepc/src/views/login/register.vue
@@ -77,18 +77,25 @@
         </div>
       </div>
     </div>
-    <div class="reg_footer mauto">
+    <copyright>
+      
+    </copyright>
+    <!-- <div class="copyright mauto">
       <div class="footer_text">
         <p>© 2005-2020 礼舍网 版权所有，并保留所有权利。All rights Reserved</p>
         <p>ICP备案证书号:粤ICP备15033641号-1</p>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
+import copyright from '../../components/copyright'
 export default {
   name: "register",
+  components:{  
+    copyright
+  },  
   data() {
     return {
       phone: "", //手机号
@@ -386,19 +393,19 @@ export default {
   background: #cc342d;
 }
 /* 底部 */
-.reg_footer {
-  p {
-    height: 14px;
-    line-height: 14px;
-    margin-bottom: 10px;
-  }
-  .footer_text {
-    padding-top: 30px;
-    padding-bottom: 30px;
-    text-align: center;
-    font-size: 14px;
-    color: #666666;
-    width: 100%;
-  }
-}
+// .copyright {
+//   p {
+//     height: 14px;
+//     line-height: 14px;
+//     margin-bottom: 10px;
+//   }
+//   .footer_text {
+//     padding-top: 30px;
+//     padding-bottom: 30px;
+//     text-align: center;
+//     font-size: 14px;
+//     color: #666666;
+//     width: 100%;
+//   }
+// }
 </style>
