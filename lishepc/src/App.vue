c9a515bfaf009f34f798e1c2b44f2ad2b3c9428a
cq741798819 <741798819@qq.com>
2020-06-07 21:44:08 +0800

    结算页面完善

--- lishepc/src/App.vue
+++ lishepc/src/App.vue
@@ -1,29 +1,16 @@
 <template>
-<<<<<<< HEAD
   <div id="app">
-=======
-
-	<!-- <div id="app">
-		<head-nav v-if="$route.path=='/login'||$route.path=='/register'"></head-nav>
-		<top-Head></top-Head>
-		<headnav></headnav>
-		<router-view></router-view>
-		 <goodList></goodList>
-		<foot></foot>
-	</div> -->
-
- <div id="app">
-
->>>>>>> b951b7fd4cd2354b91771fc763f744a2185b7b4a
-    <top-Head></top-Head>
-    <head-nav></head-nav>
-    <goodList></goodList>
-    <foot></foot>
+    <top-Head v-if="$route.path!='/login'&&$route.path!='/register'"></top-Head>
+    <search v-if="$route.path!='/total'&&$route.path!='/login'&&$route.path!='/register'&&$route.path!='/cart'"></search>
+    <head-nav v-if="$route.path!='/login'&&$route.path!='/register'&&$route.path!='/total'&&$route.path!='/cart'"></head-nav>
+    <router-view></router-view>
+    <foot  v-if="$route.path!='/login'&&$route.path!='/register'"></foot>
   </div>
 </template>
 <script>
 import topHead from "./components/topHead";
-import goodList from "./views/productList/goodList";
+import search from './components/headSearch'
+// import goodList from "./views/productList/goodList";
 import headNav from "_c/headNav.vue";
 import foot from "./components/footer";
 export default {
@@ -32,30 +19,12 @@ export default {
     headNav,
     topHead,
     foot,
-    goodList
-<<<<<<< HEAD
+    search
+    // goodList
   }
 };
-=======
-
-}}
->>>>>>> b951b7fd4cd2354b91771fc763f744a2185b7b4a
 </script>
-
 <style lang="less">
-<<<<<<< HEAD
 @import url("./assets/css/base.less");
-=======
-
-	@import url("./assets/css/base.less");
-
-	#app {
-		min-width: 1200px;
-	}
-
-	body {
-		background: #f0f0f0;
-	}
->>>>>>> b951b7fd4cd2354b91771fc763f744a2185b7b4a
 </style>
 
--- lishepc/src/components/topHead.vue
+++ lishepc/src/components/topHead.vue
@@ -52,32 +52,10 @@
           <li class="focus" @mouseover="showFollow" @mouseout="hideFollow">
             <a href="#">关注我们</a>
             <div class="follow" ref="follow">
-              <img src="../../assets/img/index/share.png" alt />
+              <img src="../assets/img/index/share.png" alt />
             </div>
           </li>
         </ul>
-      </div>
-    </div>
-    <div class="head-search">
-      <div class="search">
-        <div class="logo">
-          <a href="#">
-            <img src="../../assets/img/index/logo_two.png" alt />
-          </a>
-        </div>
-        <div class="search-box">
-          <input type="text" placeholder="八马茶叶" />
-          <a href="#">
-            <i class="searchIput"></i>
-          </a>
-        </div>
-        <div class="cart">
-          <a href="#">
-            <img src="../../assets/img/index/cart.png" alt />
-          </a>
-          <p class="ShoppingCart">我的购物车</p>
-          <span class="ling">0</span>
-        </div>
       </div>
     </div>
   </div>
@@ -100,9 +78,6 @@ export default {
   }
 };
 </script>
-
-
-
 <style lang="less" scope>
 .head-nav {
   height: 30px;
@@ -160,7 +135,7 @@ export default {
             right: 5px;
             vertical-align: middle;
             transform: translateY(-50%) rotate(180deg);
-            background-image: url(../../assets/img/index/top_ic_fold.png);
+            background-image: url(../assets/img/index/top_ic_fold.png);
             background-size: contain;
             background-repeat: no-repeat;
             background-position: center center;
@@ -217,102 +192,5 @@ export default {
     }
   }
 }
-  .head-search {
-    background: rgb(240, 240, 240);
-
-    .search {
-      width: 1200px;
-      height: 120px;
-      margin: 0 auto;
-      position: relative;
-      display: flex;
-      align-items: center;
-
-      .logo {
-        width: 188px;
-        height: 120px;
-        display: flex;
-        align-items: center;
-        float: left;
-      }
-
-      .search-box {
-        width: 520px;
-        height: 40px;
-        border: 2px solid rgb(255, 78, 76);
-        border-radius: 20px;
-        overflow: hidden;
-        margin-left: 100px;
-        position: relative;
-
-        input {
-          width: 520px;
-          height: 40px;
-          padding: 0 15px;
-          font-size: 12px;
-          border: none;
-          outline: none;
-          line-height: 40px;
-        }
-
-        a {
-          width: 80px;
-          height: 40px;
-          display: inline-block;
-          position: absolute;
-          right: 0;
-          top: -2px;
-          border-radius: 20px;
-          z-index: 999;
-          background: rgb(255, 78, 76);
-
-          .searchIput {
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            width: 16px;
-            height: 16px;
-            background-image: url(../../assets/img/index/search.png);
-            background-size: cover;
-            background-repeat: no-repeat;
-            display: inline-block;
-          }
-        }
-      }
-    }
-
-    .cart {
-      display: flex;
-      align-items: center;
-      background-color: #fff;
-      border: 1px solid #f3d6d6;
-      border-radius: 20px;
-      height: 40px;
-      padding: 0 15px;
-      margin-left: 20px;
-      position: relative;
-      cursor: pointer;
-
-      .ShoppingCart {
-        font-size: 12px;
-        color: rgb(255, 78, 76);
-        margin-left: 5px;
-        margin-right: 5px;
-      }
-
-      .ling {
-        border: 1px solid #f3d6d6;
-        border-radius: 50%;
-        background: rgb(255, 78, 76);
-        color: #fff;
-        width: 20px;
-        height: 20px;
-        line-height: 1.4;
-        text-align: center;
-        font-size: 13px;
-      }
-    }
-  }
-
+ 
 </style>
